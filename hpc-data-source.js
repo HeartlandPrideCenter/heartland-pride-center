44f6c00ac3d9b3080f8c52559502b003d5f25b69
HeartlandPrideCenter <heartlandpridecenter@gmail.com>
2026-07-02 21:31:24 -0400

    Complete HPC public data config

--- hpc-data-source.js
+++ hpc-data-source.js
@@ -1,2 +1,2 @@
 window.HPC_DATA_URL = 'https://xrunawtqotumbsztboxo.supabase.co';
-window.HPC_DATA_PUBLIC_TOKEN = 'ADD_PUBLIC_TOKEN_HERE';
+window.HPC_DATA_PUBLIC_TOKEN = 'sb_publishable_UyoqGKapp6Da2CNE4qqlZA_BodqSra8';
