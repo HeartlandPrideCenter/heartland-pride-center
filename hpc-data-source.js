8beeb5ecf0eca5ce7c8d40fb95f9f3a087601de2
HeartlandPrideCenter <heartlandpridecenter@gmail.com>
2026-07-07 15:34:46 -0400

    Load Business Network cards helper

--- hpc-data-source.js
+++ hpc-data-source.js
@@ -1,6 +1,13 @@
 window.HPC_DATA_URL = 'https://xrunawtqotumbsztboxo.supabase.co';
 window.HPC_DATA_PUBLIC_TOKEN = 'sb_publishable_UyoqGKapp6Da2CNE4qqlZA_BodqSra8';
 
+if (window.location.pathname.includes('business-network')) {
+  const cardsScript = document.createElement('script');
+  cardsScript.src = 'land-of-hearts-cards-view.js?v=show-stage-20260707';
+  cardsScript.defer = true;
+  document.head.appendChild(cardsScript);
+}
+
 if (window.location.pathname.includes('staff')) {
   const version = 'v2-20260704-1';
   const scripts = [
